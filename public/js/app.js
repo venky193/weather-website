2adc62960a35f29179dde965beab84029477a554
Venkatesh Khanna <VK00497465@techmahindra.com>
2020-01-30 00:03:08 +0530

    Removed unnecessary Console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client Side JavaScript File is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
